af3d6df061b1b0fb435906e02c4e0cd8319ae4f8
Murray Cumming <murrayc@src.gnome.org>
2007-11-19 14:21:36 +0000

    checking in changes to test
    svn path=/cluttermm/trunk/; revision=1231

--- tests/test-alpha-creation.cc
+++ tests/test-alpha-creation.cc
@@ -1,4 +1,6 @@
 #include <cluttermm.h>
+#include <clutter/clutter.h>
+#include <iostream>
 
 int
 main (int argc, char *argv[])
@@ -10,6 +12,12 @@ main (int argc, char *argv[])
         Clutter::Timeline::create (360, 60); // num frames, fps
   Glib::RefPtr<Clutter::Alpha> alpha =
     Clutter::Alpha::create (timeline, Clutter::ALPHA_SINE);
+
+  Glib::RefPtr<Clutter::BehaviourScale> scaler_1 =
+    Clutter::BehaviourScale::create (alpha, 0.5, 1.0, Clutter::GRAVITY_CENTER);
+
+  //The refcount should probably be 2 here (it is):
+  std::cout << "alpha ref_count=" << G_OBJECT(alpha->gobj())->ref_count << std::endl;
   
   return 0;
 }
