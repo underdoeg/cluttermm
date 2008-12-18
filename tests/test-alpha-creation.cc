abb6d251574570a68142a22e9ec76edd61ce86a4
Daniel Elstner <daniel@src.gnome.org>
2008-12-18 13:04:36 +0000

    Kill cruft. The test case still seems a bit odd though since it creates a
    * tests/test-alpha-creation.cc: Kill cruft.  The test case still
    seems a bit odd though since it creates a Behaviour which is never
    used.
    
    svn path=/cluttermm/trunk/; revision=1881

--- tests/test-alpha-creation.cc
+++ tests/test-alpha-creation.cc
@@ -1,40 +1,30 @@
 #include <cluttermm.h>
-#include <clutter/clutter.h>
-#include <iostream>
 
-Glib::RefPtr<Clutter::Actor> hand;
-
-int
-main (int argc, char *argv[])
+int main(int argc, char** argv)
 {
-    // initialize the C++ wrapper types
-    Clutter::init(&argc, &argv);
+  // initialize the C++ wrapper types
+  Clutter::init(&argc, &argv);
 
-    Glib::RefPtr<Clutter::Stage> stage = Clutter::Stage::get_default ();
+  Glib::RefPtr<Clutter::Stage> stage = Clutter::Stage::get_default();
 
-    // Create a timeline to manage animation
-    Glib::RefPtr<Clutter::Timeline> timeline =
-        Clutter::Timeline::create (360, 60); // num frames, fps
+  // Create a timeline to manage animation
+  Glib::RefPtr<Clutter::Timeline> timeline =
+      Clutter::Timeline::create(360, 60); // num frames, fps
 
-    // Set up some behaviours to handle scaling
-    //Causes a Gobject (refcount?) warning: Glib::RefPtr<Clutter::Alpha> alpha =
-    //    Clutter::Alpha::create (timeline, Clutter::ALPHA_SINE);
-    Glib::RefPtr<Clutter::Alpha> alpha =
-        Clutter::Alpha::create(timeline, &Clutter::Alpha::sine_func);
+  // Set up some behaviours to handle scaling
+  Glib::RefPtr<Clutter::Alpha> alpha =
+      Clutter::Alpha::create(timeline, &Clutter::Alpha::sine_func);
 
-    Glib::RefPtr<Clutter::BehaviourScale> scaler_1 =
-        Clutter::BehaviourScale::create (alpha,
-                0.5, 0.5,
-                1.0, 1.0);
+  Glib::RefPtr<Clutter::BehaviourScale> scaler_1 =
+      Clutter::BehaviourScale::create(alpha, 0.5, 0.5, 1.0, 1.0);
 
-    // Show everying ( and map window )
-    stage->show ();
+  // Show everying ( and map window )
+  stage->show();
 
-    // and start it
-    timeline->start ();
+  // and start it
+  timeline->start();
 
-    clutter_main ();
+  Clutter::main();
 
-    return 0;
+  return 0;
 }
-
