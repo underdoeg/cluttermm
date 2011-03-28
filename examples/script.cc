dc636cc3badb1879e0396b9e7fd41614d18431df
Chris Kühl <chrisk@openismus.com>
2011-03-28 15:50:56 +0200

    Updated Clutter::Script example to use Clutter::Script::get_object.
    * example/script.cc: Using Clutter::Script::get_object to avoid an
    ugly cast.

--- examples/script.cc
+++ examples/script.cc
@@ -1,4 +1,5 @@
 #include <cluttermm.h>
+#include <iostream>
 
 // This is the JSON node. Notice that all types are C-types.
 // One can not currenty use C++-types.
@@ -6,28 +7,34 @@ static const Glib::ustring jsonNode("[                   \
   {                                                      \
     \"id\" : \"stage\",                                  \
     \"type\" : \"ClutterStage\",                         \
-    \"width\" : 200,                                     \
-    \"height\" : 200,                                    \
+    \"width\" : 300,                                     \
+    \"height\" : 300,                                    \
     \"color\" : \"#20456f\",                             \
+    \"children\" : [ \"mybox\"]                          \
+  },                                                     \
+  {                                                      \
+    \"id\" : \"mybox\",                                  \
+    \"type\" : \"ClutterBox\",                           \
+    \"width\" : 300,                                     \
+    \"height\" : 300,                                    \
+    \"layout-manager\": {                                \
+               \"type\"  : \"ClutterFlowLayout\"         \
+    },                                                   \
     \"children\" : [ \"text-box\", \"rect1\", \"rect2\"] \
   },                                                     \
   {                                                      \
     \"id\" : \"rect1\",                                  \
     \"type\" : \"ClutterRectangle\",                     \
-    \"width\" :  100,                                    \
+    \"width\" : 100,                                     \
     \"height\" : 100,                                    \
-    \"x\" : 10,                                          \
-    \"y\" : 10,                                          \
     \"color\" : \"#523202\",                             \
     \"opacity\" : 127                                    \
   },                                                     \
   {                                                      \
     \"id\" : \"rect2\",                                  \
     \"type\" : \"ClutterRectangle\",                     \
-    \"width\" :  100,                                    \
+    \"width\" : 100,                                     \
     \"height\" : 100,                                    \
-    \"x\" : 50,                                          \
-    \"y\" : 50,                                          \
     \"color\" : \"#cc99ee\"                              \
   },                                                     \
   {                                                      \
@@ -35,31 +42,42 @@ static const Glib::ustring jsonNode("[                   \
     \"type\" : \"ClutterText\",                          \
     \"width\" : 100,                                     \
     \"height\" : 60,                                     \
-    \"x\" : 20,                                          \
-    \"y\" : 20,                                          \
     \"text\" : \"This is text!\",                        \
     \"color\" : \"#ddeeee\"                              \
-  }                                                      \
+} \
 ]");
 
 int main(int argc, char** argv)
 {
-  Clutter::init(&argc, &argv);
+  try
+  {
+    Clutter::init(&argc, &argv);
 
-  Glib::RefPtr<Clutter::Script> script = Clutter::Script::create();
+    Glib::RefPtr<Clutter::Script> script = Clutter::Script::create();
 
-  // Load the JSON node from a string.
-  script->load_from_data(jsonNode);
+    // Load the JSON node from a string.
+    script->load_from_data(jsonNode);
 
-  // Get the stage by name, casting it to the correct C++-type.
-  const Glib::RefPtr<Clutter::Stage> stage =
-    Glib::RefPtr<Clutter::Stage>::cast_static(script->get_object("stage"));
+    // Get the stage by name, casting it to the correct C++-type.
+    Glib::RefPtr<Clutter::Stage> stage;
+    script->get_object("stage", stage);
 
-  // Show the stage:
-  stage->show();
+    Glib::RefPtr<Clutter::Box> box;
+    script->get_object("mybox", box);
 
-  // Start the main loop, so we can respond to events:
-  Clutter::main();
+    std::cout << "Box->width: "  << box->get_width() << std::endl;
+
+    // Show the stage:
+    stage->show();
+
+    // Start the main loop, so we can respond to events:
+    Clutter::main();
+  }
+  catch (const Glib::Error& error)
+  {
+      std::cerr << "Exception: " << error.what() << std::endl;
+      return 1;
+  }
 
   return 0;
 }
