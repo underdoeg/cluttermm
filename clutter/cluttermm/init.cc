c6a84189d5b24375ad0d13da87bd531a7187369b
Murray Cumming <murrayc@murrayc.com>
2014-04-17 09:27:18 +0200

    Init: Uncomment the init() overload that takes an OptionContext.

--- clutter/cluttermm/init.cc
+++ clutter/cluttermm/init.cc
@@ -53,10 +53,6 @@ void init(int& argc, gchar**& argv)
   init(&argc, &argv);
 }
 
-// TODO: This does not work yet because because of clutter bug #1033. It has
-// already been fixed in trunk but there was no release since. We can
-// uncomment this as soon as clutter with that fix has been released.
-#if 0
 void init(int* argc, gchar** argv[], Glib::OptionContext& option_context)
 {
   common_init();
@@ -64,15 +60,18 @@ void init(int* argc, gchar** argv[], Glib::OptionContext& option_context)
   add_clutter_option_group(option_context);
   option_context.parse(*argc, *argv);
   // option_context_parse already parsed the clutter arguments:
-  clutter_init_with_args(NULL, NULL, NULL, NULL, NULL, &error);
-  if(error != NULL) Glib::Error::throw_exception(error);
+  const ClutterInitError result = clutter_init_with_args(NULL, NULL, NULL, NULL, NULL, &error);
+  if(error)
+    Glib::Error::throw_exception(error);
+
+  if(result != CLUTTER_INIT_SUCCESS)
+    throw Glib::Error(); //This should never happen. The GError should always be set.
 }
 
 void init(int& argc, gchar**& argv, Glib::OptionContext& option_context)
 {
   init(&argc, &argv, option_context);
 }
-#endif
 
 void add_clutter_option_group(Glib::OptionContext& option_context)
 {
